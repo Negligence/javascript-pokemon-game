133f2732aee0250e3ff3ca768fd56b33d1b97866
Negligence <janpanado@gmail.com>
2022-08-17 22:05:04 +0800

    Finally Made Touch Events for Mobile

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ battleZonesMap.forEach((row, i) => {
         position: {
           x: j * Boundary.width + offset.x,
           y: i * Boundary.height + offset.y
-        }}));
+        }})); 
       });
     });
 
@@ -130,7 +130,19 @@ const keys = {
   },
   ArrowRight: {
     pressed: false
-  }
+  },
+  TouchUp: {
+    pressed: false
+  },
+  TouchLeft: {
+    pressed: false
+  },
+  TouchDown: {
+    pressed: false
+  },
+  TouchRight: {
+    pressed: false
+  },
 }
 
 // const testBoundary = new Boundary({
@@ -170,24 +182,11 @@ function animate() {
   if (keys.w.pressed || keys.ArrowUp.pressed || keys.a.pressed || keys.ArrowLeft.pressed || keys.s.pressed || keys.ArrowDown.pressed || keys.d.pressed || keys.ArrowRight.pressed) {
     for (let i = 0; i < battleZones.length; i++) {
       const battleZone = battleZones[i];
-      const overlappingArea =
-      (Math.min(player.position.x + player.width,
-        battleZone.position.x + battleZone.width) - 
-      Math.max(player.position.x,
-        battleZone.position.x)) * (
-      Math.min(player.position.y + player.height,
-        battleZone.position.y + battleZone.heights
-      ) - Math.max(player.position.y,
-        battleZone.position.y));
-      if (
-        rectangularCollision({
-          rectangle1: player,
-          rectangle2: battleZone
-        }) /* &&
-        overlappingArea > (player.width * player.height) / 2 */
-      ) {
-        console.log(overlappingArea);
-        console.log('Battle Zone Collision! Fight 💥');
+      const overlappingArea = (Math.min(player.position.x + player.width, battleZone.position.x + battleZone.width) - Math.max(player.position.x, battleZone.position.x)) * (Math.min(player.position.y + player.height, battleZone.position.y + battleZone.height) - Math.max(player.position.y, battleZone.position.y));
+
+      if (rectangularCollision({rectangle1: player, rectangle2: battleZone}) && overlappingArea > (player.width * player.height) / 2 && Math.random() < 0.01) {
+        console.log(`collisioin!`);
+
         break;
       }      
     }
@@ -196,7 +195,7 @@ function animate() {
   let moving = true;
   player.moving = false;
   
-  if (keys.w.pressed && lastKey === 'w' || keys.ArrowUp.pressed && lastKey === 'ArrowUp') {
+  if (keys.w.pressed && lastKey === 'w' || keys.ArrowUp.pressed && lastKey === 'ArrowUp' || keys.TouchUp.pressed && lastKey === `TouchUp`) {
     for (let i = 0; i < boundaries.length; i++) {
       player.moving = true;
       player.image = player.sprites.up;
@@ -223,7 +222,7 @@ function animate() {
     movable.position.y += speed
   })}
 
-  else if (keys.a.pressed && lastKey === 'a' || keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') {
+  else if (keys.a.pressed && lastKey === 'a' || keys.ArrowLeft.pressed && lastKey === 'ArrowLeft' || keys.TouchLeft.pressed && lastKey === `TouchLeft`) {
     for (let i = 0; i < boundaries.length; i++) {
       player.moving = true;
       player.image = player.sprites.left;
@@ -250,7 +249,7 @@ function animate() {
     movable.position.x += speed
   })}
 
-  else if (keys.s.pressed && lastKey === 's' || keys.ArrowDown.pressed && lastKey === 'ArrowDown') {
+  else if (keys.s.pressed && lastKey === 's' || keys.ArrowDown.pressed && lastKey === 'ArrowDown' || keys.TouchDown.pressed && lastKey === `TouchDown`) {
     for (let i = 0; i < boundaries.length; i++) {
       player.moving = true;
       player.image = player.sprites.down;
@@ -277,7 +276,7 @@ function animate() {
     movable.position.y -= speed
   })}
 
-  else if (keys.d.pressed && lastKey === 'd' || keys.ArrowRight.pressed && lastKey === 'ArrowRight') {
+  else if (keys.d.pressed && lastKey === 'd' || keys.ArrowRight.pressed && lastKey === 'ArrowRight' || keys.TouchRight.pressed && lastKey === `TouchRight`) {
     for (let i = 0; i < boundaries.length; i++) {
       player.moving = true;
       player.image = player.sprites.right;
@@ -396,4 +395,69 @@ window.addEventListener('keyup', (e) => {
     default:
       break;
   }
-})
+});
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(event) {
+  return event.touches ||             // browser API
+         event.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(event) {
+    const firstTouch = getTouches(event)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+}                                     
+                                                                         
+function handleTouchMove(event) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = event.touches[0].clientX;                                    
+    var yUp = event.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+
+            keys.TouchLeft.pressed = true;
+            lastKey = `TouchLeft`;
+        } else {
+            /* left swipe */
+            keys.TouchRight.pressed = true;
+            lastKey = `TouchRight`;
+        }         
+
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+            keys.TouchUp.pressed = true;
+            lastKey = `TouchUp`;
+
+        } else { 
+            /* up swipe */
+            keys.TouchDown.pressed = true;
+            lastKey = `TouchDown`;
+        }                                                                 
+    }
+    /* reset values */
+    // xDown = null;
+    // yDown = null;                                             
+}
+
+function handleTouchEnd () {
+  keys.TouchLeft.pressed = false;
+  keys.TouchRight.pressed = false;
+  keys.TouchUp.pressed = false;
+  keys.TouchDown.pressed = false;
+}
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener('touchend', handleTouchEnd, false);
